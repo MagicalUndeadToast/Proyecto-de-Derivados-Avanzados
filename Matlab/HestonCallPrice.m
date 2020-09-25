fa744f6e1fe44f427649b54d4b2ec9dd4eba9fef
MagicalUndeadToast <71853096+MagicalUndeadToast@users.noreply.github.com>
2020-09-25 20:48:33 -0300

    Update HestonCallPrice.m
    No se pudo con los simbolos, dejo su version tex.
    
    Co-Authored-By: FelipeGaldames <42103799+FelipeGaldames@users.noreply.github.com>

--- Matlab/HestonCallPrice.m
+++ Matlab/HestonCallPrice.m
@@ -9,9 +9,13 @@ dphi=0.01;
 maxphi=50;
 phi=(eps:dphi:maxphi)';
 
-% w=?
-% theta=?
-% psi=?
+% Estos simbolos no se guardan al parecer, dejo su version de latex para
+% que asi en caso de tener dudas podamos buscarlos, dado que no apareceran,
+% solo en la version local.
+
+% w=\omega
+% theta=\theta
+% psi=\psi
 
 f1 = CF_SVj(log(S),vt,T,r,q,theta*w,0.5,theta+psi-rho*sig,rho,sig,phi);
 P1 = 0.5+(1/pi)*sum(real(exp(-1i*phi*log(K)).*f1./(1i*phi))*dphi);
