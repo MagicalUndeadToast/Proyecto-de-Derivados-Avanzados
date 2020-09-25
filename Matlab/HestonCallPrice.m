832a32f6289868adf25517d2c978fd600b5ded78
MagicalUndeadToast <71853096+MagicalUndeadToast@users.noreply.github.com>
2020-09-25 20:14:07 -0300

    Update HestonCallPrice.m
    Aca esta la funcion HestonCallPrice, falta arreglar detalles al parecer.
    
    Co-Authored-By: FelipeGaldames <42103799+FelipeGaldames@users.noreply.github.com>

--- Matlab/HestonCallPrice.m
+++ Matlab/HestonCallPrice.m
@@ -1,6 +1,19 @@
-function [] = HestonCallPrice(S,K,r,f)
-%Funcion que computa el precio de la opcion 
-%   Detailed explanation goes here
+function [C] = HestonCallPrice(S,K,r,q,T,vt,kap,th,sig,rho,lda)
+%Funcion que computa el precio de la opcion a partir de la formula cerrada
+%de Heston.
+%   A partir de los argumentos ingresados, esta función regresa el precio
+%   de a partir de la fórmula cerrada de Heston, haciendo las integrales
+%   como suma y utilizando el ppt del profe.
+
+dphi=0.01;
+maxphi=50;
+phi=(eps:dphi:maxphi)';
+
+f1 = CF_SVj(log(S),vt,T,r,q,kap*th,0.5,kap+lda-rho*sig,rho,sig,phi);
+P1 = 0.5+(1/pi)*sum(real(exp(-1i*phi*log(K)).*f1./(1i*phi))*dphi);
+f2 = CF_SVj(log(S),vt,T,r,q,kap*th,-0.5,kap+lda,rho,sig,phi);
+P2 = 0.5+(1/pi)*sum(real(exp(-1i*phi*log(K)).*f2./(1i*phi))*dphi);
+C = S*exp(-q*T)*P1 -K*exp(-r*T)*P2;
 
 end
 
