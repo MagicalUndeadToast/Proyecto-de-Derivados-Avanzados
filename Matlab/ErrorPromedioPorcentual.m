a4cbbe7caad90ca2e2067b0a0deef9e88ac41769
MagicalUndeadToast <71853096+MagicalUndeadToast@users.noreply.github.com>
2020-10-13 23:22:37 -0300

    Create ErrorPromedioPorcentual.m
    se creo :V

---
+++ Matlab/ErrorPromedioPorcentual.m
@@ -0,0 +1,9 @@
+function [ErrorPromedioPorcentual] = ...
+    ErrorPromedioPorcentual(Empirica,Teorica)
+%Funcion hecha casi al final para calcular el error medio absoluto.
+%   Al poner dos matrices calcula el error entre ellas, para poder usarse
+%   en la consola o en comprobaciones rapidas.
+ErrorPromedioPorcentual=mean2(abs(Empirica-Teorica))...
+    /mean2(abs(Teorica));
+end
+
