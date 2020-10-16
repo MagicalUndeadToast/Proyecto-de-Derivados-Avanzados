cea09a665a81dcf7dcbddfd1f58efc397c7a4b3b
MagicalUndeadToast <71853096+MagicalUndeadToast@users.noreply.github.com>
2020-10-16 01:27:47 -0300

    Update ParteIII.m
    asdasadsasd

--- Matlab/ParteIII.m
+++ Matlab/ParteIII.m
@@ -61,50 +61,52 @@ x0 = [0.1, 0.01, 0.21, 0.5,0.5]; %Parametros iniciales NO TOCAAAAAR
 % NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR 
 
 % CALIBRAMOS PARA EL DIA 1
-for k=1:1
-%     fun = @(x)abs(HestonCallPrice(Spot(k,1),Strike(k,1),r(k,1),...
-%         q(k,1),Tiempo(k,1),x(1),x(2),x(3),x(4),x(5),x(2)*x(3))...
-%         -OptionValue(k,1));
-%     V0=HestonCallPrice(Spot(k,1),Strike(k,1),r(k,1),...
-%         q(k,1),Tiempo(k,1),x(1),x(2),x(3),x(4),x(5),x(2)*x(3));
+% for k=1:1
+% %     fun = @(x)abs(HestonCallPrice(Spot(k,1),Strike(k,1),r(k,1),...
+% %         q(k,1),Tiempo(k,1),x(1),x(2),x(3),x(4),x(5),x(2)*x(3))...
+% %         -OptionValue(k,1));
+% %     V0=HestonCallPrice(Spot(k,1),Strike(k,1),r(k,1),...
+% %         q(k,1),Tiempo(k,1),x(1),x(2),x(3),x(4),x(5),x(2)*x(3));
+% %     
+% %     fun= @(x)abs(dummyFunction(Spot(1,1),r(k,1),q(k,1),Tiempo(k,1),Strike(k,1),...
+% %         HestonCallPrice(Spot(1,1),Strike(k,1),r(k,1),...
+% %         q(k,1),Tiempo(k,1),x(1),x(2),x(3),x(4),x(5),x(2)*x(3))...
+% %         ,0.1,0.1/100,1)-Sigma(k,1));
 %     
-%     fun= @(x)abs(dummyFunction(Spot(1,1),r(k,1),q(k,1),Tiempo(k,1),Strike(k,1),...
-%         HestonCallPrice(Spot(1,1),Strike(k,1),r(k,1),...
-%         q(k,1),Tiempo(k,1),x(1),x(2),x(3),x(4),x(5),x(2)*x(3))...
-%         ,0.1,0.1/100,1)-Sigma(k,1));
-    
-    fun=@(x)ErrorPromedio(Sigma(1,:),FuncionAux(Spot,Strike,r...
-        ,q,Tiempo,x(1),x(2),x(3),x(4),x(5))) 
-    
-    lb = [0, 0, 0, 0, -.9];
-    ub = [1, 100, 1, .5, .9];
-    %options = optimset('Display','none');
-    %x = fminsearch(fun,x0,options);
-%     options = optimoptions('fmincon','Display','none');
-%     x = fmincon(fun,x0,[],[],[],[],lb,ub,options);
-    x = fmincon(fun,x0,[],[],[],[],lb,ub);
-    
-    vt=x(1);
-    theta=x(2);
-    w=x(3);
-    sig=x(4);
-    rho=x(5);
-    psi=x(2)*x(3);
-    x0=x;
-    
-    parametros(1,k)=x(1);
-    parametros(2,k)=x(2);
-    parametros(3,k)=x(3);
-    parametros(4,k)=x(4);
-    parametros(5,k)=x(5);
-    parametros(6,k)=x(2)*x(3);
-    
-%     %ValorEmpirico(k,1)=HestonCallPrice(Spot(k,1),Strike(k,1),r(k,1),...
-%         q(k,1),Tiempo(k,1),vt,theta,w,sig,rho,psi);
+%     fun=@(x)ErrorPromedio(Sigma(1,:),FuncionAux(Spot,Strike,r...
+%         ,q,Tiempo,x(1),x(2),x(3),x(4),x(5))) 
+%     
+%     lb = [0, 0, 0, 0, -.9];
+%     ub = [1, 100, 1, .5, .9];
+%     %options = optimset('Display','none');
+%     %x = fminsearch(fun,x0,options);
+% %     options = optimoptions('fmincon','Display','none');
+% %     x = fmincon(fun,x0,[],[],[],[],lb,ub,options);
+%     x = fmincon(fun,x0,[],[],[],[],lb,ub);
+%     
+%     vt=x(1);
+%     theta=x(2);
+%     w=x(3);
+%     sig=x(4);
+%     rho=x(5);
+%     psi=x(2)*x(3);
+%     x0=x;
+%     
+%     parametros(1,k)=x(1);
+%     parametros(2,k)=x(2);
+%     parametros(3,k)=x(3);
+%     parametros(4,k)=x(4);
+%     parametros(5,k)=x(5);
+%     parametros(6,k)=x(2)*x(3);
+%     
+% %     %ValorEmpirico(k,1)=HestonCallPrice(Spot(k,1),Strike(k,1),r(k,1),...
+% %         q(k,1),Tiempo(k,1),vt,theta,w,sig,rho,psi);
+% 
+%     SigmaEmpirico=FuncionAux(Spot,Strike,r,q,Tiempo,x(1),x(2),x(3),x(4),x(5))
+% end
 
-    SigmaEmpirico=FuncionAux(Spot,Strike,r,q,Tiempo,x(1),x(2),x(3),x(4),x(5))
-end
-
+[parametrosPD,SigmaEmpiricoPD,tfinalPD,tpromedioPD] = ...
+    DBDCalibration(Sigma,Spot,Strike,r,q,Tiempo,x0,'Primer Dia');
 
 %% 
 figure(1);
