b37a8d0aab5e2fab687f114a74e9097865e03eec
MagicalUndeadToast <71853096+MagicalUndeadToast@users.noreply.github.com>
2020-10-18 23:28:17 -0300

    sadasdasd
    asdasdas

--- Matlab/Proyecto.m
+++ Matlab/Proyecto.m
@@ -287,6 +287,220 @@ disp("El error promedio del fair value entre BS y Monte-Carlo es de un: "...
     + ErrorBSMC*100+"%")
 disp("El error promedio del fair value entre BS y Heston es de un: "...
     + ErrorBSHeston*100+"%")
+disp(" ")
+
+%% Decimoseptima Seccion.
+% Justificacion de los parametros.
+% Parametros
+
+vt=0.2^2;
+theta=0.015;
+w=0.01;
+sig=0.5;
+rho=0.05;
+psi=theta.*w;
+
+X0Inicial=ParametrosIniciales(Sigma,Spot);
+
+%% Decimooctaba Seccion.
+% Calibramos para el primer dia.
+% Parametros iniciales
+x0 = [0.1, 0.01, 0.21, 0.4,0.5]; %Parametros iniciales NO TOCAAAAAR
+% NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR 
+
+% Funcion que devuelve los parametros ,sigmas y precios calculados
+% con nuestro modelo. 
+[parametrosPD,SigmaEmpiricoPD,tfinalPD,tpromedioPD,OptionValueEmpiricoPD] = ...
+    DBDCalibration(Sigma,Spot,Strike,r,q,Tiempo,x0,'Primer Dia');
+
+%% Decimonovena Seccion.
+% Se calculan los diferentes errores para los sigmas y precios para Primer Dia.
+SigmaErrorPD=ErrorPromedio(SigmaEmpiricoPD,Sigma(1,:));
+PriceErrorPD=ErrorPromedio(OptionValueEmpiricoPD,OptionValue(1,:));
+SigmaPercentErrorPD=ErrorPromedioPorcentual(SigmaEmpiricoPD,Sigma(1,:));
+PriceErrorPercentPD=ErrorPromedioPorcentual(OptionValueEmpiricoPD,OptionValue(1,:));
+RowSigmaErrorPD=ErrorFila(SigmaEmpiricoPD,Sigma(1,:));
+RowPriceErrorPD=ErrorFila(OptionValueEmpiricoPD,OptionValue(1,:));
+
+%% Vigesima Seccion.
+% Se imprimen los errores de calibracion  del primer dia.
+disp("El error promedio de la volatilidad para el primer dia es: " ...
+    + SigmaErrorPD)
+disp("El error promedio porcentual de la volatilidad para el primer dia es: " ...
+    + SigmaPercentErrorPD + "%")
+disp(" ")
+disp("El error promedio del valor de la  opcion para el primer dia es: " ...
+    + PriceErrorPD)
+disp("El error promedio porcentual del valor de la opcion para el primer dia es: " ...
+    + PriceErrorPercentPD + "%")
+disp(" ")
+
+%% Vigesimoprimera Seccion.
+% Graficos de la Smile del primer dia en 2D.
+Tenores=["1 Mes", "3 Meses", "6 Meses", "9 Meses", "12 Meses" ];
+for i=1:5
+    if i==1
+        k=1;
+    end
+figure(i);
+plotaux=[SigmaEmpiricoPD(1,k),SigmaEmpiricoPD(1,k+1),SigmaEmpiricoPD(1,k+2),...
+    SigmaEmpiricoPD(1,k+3),SigmaEmpiricoPD(1,k+4)];
+plotaux2=[Sigma(1,k),Sigma(1,k+1),Sigma(1,k+2),Sigma(1,k+3),Sigma(1,k+4)];
+plotaux3=linspace(1,5,5);
+plot(plotaux3,plotaux),title(Tenores(i));
+hold on;
+plot(plotaux3,plotaux2);
+legend("Smile Empirica","Smile Teorica");
+xlabel("Pilares");
+ylabel("Volatilidad");
+xticks([1,2,3,4,5]);
+xticklabels({'10P','25P','50C','75C','90C'});
+hold off;
+k=i*5+1;
+
+end
+
+%% Vigesimosegunda Seccion.
+% Calibramos para datos entre TInicial y TFinal.
+% Parametros iniciales para empezar a calibrar.
+x0 = [0.1, 0.01, 0.21, 0.4,0.5]; %Parametros iniciales NO TOCAAAAAR
+% NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR
+tinicial=1;
+tfinal=3;
+[parametrosFC,SigmaEmpiricoFC,tfinalFC,tpromedioFC,OptionValueEmpiricoFC] = ...
+    DBDCalibration(Sigma,Spot,Strike,r,q,Tiempo,x0,'Fechas',tinicial,tfinal);
+
+%% Vigesimotercera Seccion.
+% Se calculan los diferentes errores para los sigmas y precios para Fechas.
+SigmaErrorFC=ErrorPromedio(SigmaEmpiricoFC,Sigma(tinicial:tfinal,:));
+PriceErrorFC=ErrorPromedio(OptionValueEmpiricoFC,OptionValue(tinicial:tfinal,:));
+SigmaPercentErrorFC=ErrorPromedioPorcentual(SigmaEmpiricoFC,Sigma(tinicial:tfinal,:));
+PriceErrorPercentFC=ErrorPromedioPorcentual(OptionValueEmpiricoFC,OptionValue(tinicial:tfinal,:));
+RowSigmaErrorFC=ErrorFila(SigmaEmpiricoFC,Sigma(tinicial:tfinal,:));
+RowPriceErrorFC=ErrorFila(OptionValueEmpiricoFC,OptionValue(tinicial:tfinal,:));
+
+%% Vigesimocuarta Seccion.
+% SMILE de la Data en 3D incompleta.
+Tenores=["1 Mes", "3 Meses", "6 Meses", "9 Meses", "12 Meses" ];
+for i=1:5
+    if i==1
+        k=1;
+    end  
+figure(i+5);
+plotaux=[SigmaEmpiricoFC(:,k),SigmaEmpiricoFC(:,k+1),SigmaEmpiricoFC(:,k+2),...
+    SigmaEmpiricoFC(:,k+3),SigmaEmpiricoFC(:,k+4)];
+plotaux2=[Sigma(:,k),Sigma(:,k+1),Sigma(:,k+2),Sigma(:,k+3),Sigma(:,k+4)];
+plotaux3=linspace(1,5,5);
+
+[X,Y]=meshgrid(plotaux3,Date(tinicial:tfinal));
+Z=plotaux2(tinicial:tfinal,:);
+surf(X,Y,Z,'FaceColor','r');
+xticks([1,2,3,4,5]);
+xticklabels({'10P','25P','50C','75C','90C'}),ylabel("Fecha")
+xlabel("Pilar"), zlabel("Volatilidad"),title(Tenores(i))
+hold on
+[X1,Y1]=meshgrid(plotaux3,Date);
+Z1=plotaux;
+surf(X,Y,Z1,'FaceColor','b');
+legend("Smile Teorica","Smile Empirica")
+hold off;
+k=i*5+1;
+end
+
+%% Vigesimoquinta Seccion.
+% Plot de la Evolucion de los Parametros.
+% Parametros a graficar evolucion fechas.
+NombreParametros=["vt", "theta", "w", "sig", "rho", "psi"];
+for i=1:6
+figure(15+i);
+plotaux=parametrosFC(:,i);
+plotaux2=Date(tinicial:tfinal);
+plot(plotaux2,plotaux);
+hold on;
+title("Evolucion del Parametro "+NombreParametros(i));
+xlabel("Fechas");   
+ylabel(NombreParametros(i));
+legend(NombreParametros(i));
+hold off;
+end
+
+%% Vigesimosexta Seccion.
+% Calibramos toda la Data.
+
+% Parametros iniciales
+x0 = [0.1, 0.01, 0.21, 0.4,0.5]; %Parametros iniciales NO TOCAAAAAR
+% NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR NO TOCAAAAAR
+
+[parametrosCP,SigmaEmpiricoCP,tfinalCP,tpromedioCP,OptionValueEmpiricoCP] = ...
+    DBDCalibration(Sigma,Spot,Strike,r,q,Tiempo,x0,'Completo');
+
+%% Vigesimoseptima Seccion.
+% Se calculan los diferentes errores para los sigmas y precios.
+SigmaErrorCP=ErrorPromedio(SigmaEmpiricoCP,Sigma);
+PriceErrorCP=ErrorPromedio(OptionValueEmpiricoCP,OptionValue);
+SigmaPercentErrorCP=ErrorPromedioPorcentual(SigmaEmpiricoCP,Sigma);
+PriceErrorPercentCP=ErrorPromedioPorcentual(OptionValueEmpiricoCP,OptionValue);
+RowSigmaErrorCP=ErrorFila(SigmaEmpiricoCP,Sigma);
+RowPriceErrorCP=ErrorFila(OptionValueEmpiricoCP,OptionValue);
+
+%% Vigesimoctava Seccion.
+% SMILE 3d Completa.
+Tenores=["1 Mes", "3 Meses", "6 Meses", "9 Meses", "12 Meses" ];
+for i=1:5
+    if i==1
+        k=1;
+    end
+figure(i+10);
+plotaux=[SigmaEmpiricoCP(:,k),SigmaEmpiricoCP(:,k+1),SigmaEmpiricoCP(:,k+2),...
+    SigmaEmpiricoCP(:,k+3),SigmaEmpiricoCP(:,k+4)];
+plotaux2=[Sigma(:,k),Sigma(:,k+1),Sigma(:,k+2),Sigma(:,k+3),Sigma(:,k+4)];
+plotaux3=linspace(1,5,5);
+
+[X,Y]=meshgrid(plotaux3,Date(:));
+Z=plotaux2(:,:);
+surf(X,Y,Z,'FaceColor','r');
+xticks([1,2,3,4,5]);
+xticklabels({'10P','25P','50C','75C','90C'}),ylabel("Fecha")
+xlabel("Pilar"), zlabel("Volatilidad"),title(Tenores(i))
+hold on
+[X1,Y1]=meshgrid(plotaux3,Date);
+Z1=plotaux;
+surf(X,Y,Z1,'FaceColor','b');
+legend("Smile Teorica","Smile Empirica")
+hold off;
+k=i*5+1;
+end
+
+%% Vigesimonovena Seccion.
+% Plot de la Evolucion de los Parametros.
+% Parametros a graficar evolucion completa.
+NombreParametros=["vt", "theta", "w", "sig", "rho", "psi"];
+for i=1:6
+figure(21+i);
+plotaux=parametrosCP(:,i);
+plotaux2=Date(:);
+plot(plotaux2,plotaux);
+hold on;
+title("Evolucion del Parametro "+NombreParametros(i));
+xlabel("Fechas");   
+ylabel(NombreParametros(i));
+legend(NombreParametros(i));
+hold off;
+end
+
+%% Trigesima Seccion.
+% A contiuacion se muestra los errores de la data.
+% Iteracion 804 completada en 6184.1895 segundos.
+x=ErroresData();
+
+%Grafica de los errores
+figure(28)
+plot(Date,x,'linewidth',1)
+xlabel("Fecha"),ylabel("Volatilidad"), title("Error promedio en el tiempo")
+hold on
+yyaxis right
+plot(Date,Spot,'linewidth',1),ylabel("Precio Spot")
+legend({'Error promedio de la volatilidad','Precio Spot'},"location",'northeast')
 
 %% Espacio de Calculo.
 % Espacio para hacer calculos en el programa sin tener que correr la
@@ -296,4 +510,4 @@ disp("El error promedio del fair value entre BS y Heston es de un: "...
 % plot(normalize(Strike(:,1)),SigmasObtenidos(:,1),'o')
 % xlabel('Strike'), ylabel('Volatilidad implicita'), title(
 %%'Smile')
-
+%%
